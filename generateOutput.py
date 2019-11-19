c69c91952d7123ffe939cccea2752ff960ad427c
Alan He <xhe22@berkeley.edu>
2019-11-19 02:26:35 -0800

    Clean up code further...

--- generateOutput.py
+++ generateOutput.py
@@ -14,28 +14,19 @@ def dropoffLocToOutput(car_route, shortest_path_info, list_of_homes, list_of_loc
 
     for home in list_of_homes:
         home_idx = list_of_locs.index(home)
-        # print(home, home_idx)
         home = list_of_locs[home_idx]
         distDict = shortest_path_info[home_idx][1][0]
         
         minDropoff = min(car_route, key=lambda loc: distDict[loc])
         if minDropoff in dropoff_info.keys():
-            # dropoff_info[minDropoff].append(home)
             dropoff_info[minDropoff].append(home_idx)
         else:
-            # dropoff_info[minDropoff] = [home]
             dropoff_info[minDropoff] = [home_idx]
 
         minDist = distDict[minDropoff]
         walking_dist += minDist
 
     total_energy = driving_dist * 2.0 / 3.0 + walking_dist
-
-    # dropoff_result = list(dropoff_info)
-    # for idx in range(len(dropoff_result)):
-        # single_dropoff_loc = dropoff_result[idx]
-        # dropoff_result[idx] = [single_dropoff_loc, dropoff_info[single_dropoff_loc]]
-
     return [car_route, dropoff_info, total_energy]
 
 
@@ -44,7 +35,6 @@ def getDrivingDist(car_route, shortest_path_info):
 
     for i in range(len(car_route) - 1):
         loc_idx, next_loc_idx = car_route[i], car_route[i + 1]
-        # loc_idx = list_of_locs.index(loc)
         loc_dist_dict = shortest_path_info[loc_idx][1][0]
         total_driving_cost += loc_dist_dict[next_loc_idx]
 
