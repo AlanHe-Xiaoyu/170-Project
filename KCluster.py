080db80c4714d85fac83bd05f6e4278dea872729
BradyWang888 <wangshunyu@berkeley.edu>
2019-11-20 23:36:06 -0800

    commit

---
+++ KCluster.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+import random
+
+
+def kcluster(dijkstra_result, number_of_houses, list_of_houses, k):
+    x = random.randrange(0, number_of_houses, 1)
+    result = [[]*k]
+    centers = [list_of_houses[x]]
+    num_centers = 1
+    result[num_centers - 1] += [list_of_houses[x]]
+    hash = [0] * number_of_houses
+    hash[list_of_houses[x]] = 1
+    for i in range(2, k + 1):
+        max_dis = 0
+        next_center = 0
+        for j in list_of_houses:
+            if hash[j] == 1:
+                continue
+            min_dis = float('inf')
+            for k in centers:
+                if dijkstra_result[k, j] < min_dis:
+                    min_dis = dijkstra_result[k, j]
+            if min_dis > max_dis:
+                max_dis = min_dis
+                next_center = j
+        hash[next_center] = 1
+        num_centers += 1
+        centers += [next_center]
+        result[num_centers - 1] += [next_center]
+    for i in list_of_houses:
+        if hash[i] == 1:
+            continue
+        dis = float('inf')
+        cluster_to_join = 0
+        for j in range(len(centers)):
+            if dijkstra_result[centers[j], i] < dis:
+                dis = dijkstra_result[centers[j], i]
+                cluster_to_join = j
+        result[cluster_to_join] += [i]
+        hash[i] = 1
+    return result
