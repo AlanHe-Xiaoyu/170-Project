448bc665bbd82b59b7ad64213012d0ed02ca10ee
Rui Zhou <ruizhouzr@berkeley.edu>
2019-12-06 01:22:52 -0800

    annealing

--- additional_annealing.py
+++ additional_annealing.py
@@ -1,14 +1,16 @@
 from generateOutput import *
 from simanneal import Annealer
 import random
+import numpy as np
 class DTHProblem(Annealer):
     """Test annealer with a travelling salesman problem."""
     def __init__(self, initialList, shortest_path_info, list_of_homes, list_of_locs):
         self.shortest_path_info = shortest_path_info
-        self.list_of_homes = list(range(list_of_locs))
+        self.list_of_homes = list_of_homes
         self.list_of_locs = list_of_locs
         super(DTHProblem, self).__init__(initialList)
     def move(self):
+        print("one step")
         initial_energy = self.energy()
         """ 0 - add, 1-remove, 2-randomly swap"""
         add_weight = 1.0
@@ -16,42 +18,50 @@ class DTHProblem(Annealer):
         swap_weight = 1.0
         weights = []
         choices = []
-        not_included = [for i in list_of_locs if i not in self.state]
+        
+        not_included = []
+        for i in self.list_of_locs:
+            if i not in self.state:
+                not_included.append(i)
         if len(not_included) == 0:
             choices = [1,2]
             weights = [remove_weight, swap_weight]
-        else if len(self.state) == 0:
+        elif len(self.state) == 0:
             choices = [0]
             weights = [1.0]
         else:
             if len(self.state) > 1:
                 choices = [0,1,2]
+                weights = [add_weight, remove_weight, swap_weight]
             else:
-                choices [0]
-        method = random.choice(choices, weights = weights)
+                choices = [0]
+                weights = [add_weight]
+        method = np.random.choice(choices)
         if method == 2:
             a = random.randint(1, len(self.state) - 2)
             b = random.randint(1, len(self.state) - 2)
             self.state[a], self.state[b] = self.state[b], self.state[a]
-        else if method == 1:
+        elif method == 1:
             a = random.randint(1, len(self.state) - 2)
             self.state.pop(a)
-        else if method == 0:
+        else:
             a = random.choice(not_included)
             b = random.randint(1, len(not_included) - 2)
             self.state.insert(b, a)
         return self.energy() - initial_energy
         
     def energy(self):
-        _, __, e = dropoffLocToOutput(self.state, self.shortest_path_info, self.list_of_homes, self.list_of_locs)
-        return e
+        _, __, ennn = dropoffLocToOutput(self.state, self.shortest_path_info, self.list_of_homes, self.list_of_locs)
+        return ennn
         
 def runAnneal(initialList, shortest_path_info, list_of_homes, list_of_locs):
-    problem = DTHProblem(initialList, shortest_path_info, list_of_homes, list_of_locs)
+    print("Start annealing")
+    tsp = DTHProblem(initialList, shortest_path_info, list_of_homes, list_of_locs)
     tsp.set_schedule(tsp.auto(minutes=0.2))
     tsp.copy_strategy = "slice"
     res, e = tsp.anneal()
-    return res, e
+    res1, res2, en = dropoffLocToOutput(res, shortest_path_info, list_of_homes, list_of_locs)
+    return res1, res2, en
 #def makeBetter(list_locs, list_homes, start_car_loc, shortest_path_info, cur_a, cur_b, cur_energy):
 #    # First check no-drive
 #    a, b, energy = dropAllAtSoda(list_locs, list_homes, start_car_loc, shortest_path_info)
